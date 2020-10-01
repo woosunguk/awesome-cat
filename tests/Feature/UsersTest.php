c211e3cc07e58a94a39e5e1ab82378afe408fac2
Woo Sunguk <woosunguk@jiran.com>
2020-10-01 14:08:00 +0900

    Create ci.yml  - phpunit (php7.3, php8.0/pcov, xdegug, xdebug3)

---
+++ tests/Feature/UsersTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        // $response = $this->get('/users', [
+        //     'Authorization' => 'Bearer r3TFX9Qow6bNGPpXuVPcZ6hXXtG9tn0VKhGZt6eG'
+        // ]);
+
+        // $response->assertStatus(200);
+    }
+}
